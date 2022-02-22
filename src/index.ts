c11fc1d0f3834cb1161e50901b352b13b3d454b3
sjk <jkshin0903@gmail.com>
2022-02-22 22:54:20 +0900

    add another query

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import { createServer } from '@graphql-yoga/node'
 import resolvers from '../graphql/resolver'
 
+const typeDefs = `
+  type Nicolas {
+    name: String!
+    age: Int!
+    gender: String!
+  }
+
+  type Query {
+    person: Nicolas!
+  }
+`
+
 // Create your server
 const server = createServer({
   schema: {
-    typeDefs: `type Query {
-  name: String!
-}
-`,
+    typeDefs,
     resolvers,
   },
 })
